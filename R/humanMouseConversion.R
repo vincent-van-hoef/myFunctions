2168325993259130597254d86f5cf01e4abe18e1
u0049379 <u0049379@gbw-s-seq08.luna.kuleuven.be>
2018-01-31 09:45:01 +0100

    set true to falase

--- R/humanMouseConversion.R
+++ R/humanMouseConversion.R
@@ -3,7 +3,7 @@
 convertHumanGeneList <- function(x){
 human = useMart("ensembl", dataset = "hsapiens_gene_ensembl")
 mouse = useMart("ensembl", dataset = "mmusculus_gene_ensembl")
-mousex = getLDS(attributes = c("hgnc_symbol"), filters = "hgnc_symbol", values = x , mart = human, attributesL = c("mgi_symbol"), martL = mouse, uniqueRows=T)[,2]
+mousex = getLDS(attributes = c("hgnc_symbol"), filters = "hgnc_symbol", values = x , mart = human, attributesL = c("mgi_symbol"), martL = mouse, uniqueRows=FALSE)[,2]
 return(mousex)
 }
  
