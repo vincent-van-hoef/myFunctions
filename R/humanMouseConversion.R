a3f7362338e5549f81009d3561d2da3c08aee843
u0049379 <u0049379@gbw-s-seq08.luna.kuleuven.be>
2018-01-31 09:41:58 +0100

    remove unique genelists

--- R/humanMouseConversion.R
+++ R/humanMouseConversion.R
@@ -3,8 +3,7 @@
 convertHumanGeneList <- function(x){
 human = useMart("ensembl", dataset = "hsapiens_gene_ensembl")
 mouse = useMart("ensembl", dataset = "mmusculus_gene_ensembl")
-genesV2 = getLDS(attributes = c("hgnc_symbol"), filters = "hgnc_symbol", values = x , mart = human, attributesL = c("mgi_symbol"), martL = mouse, uniqueRows=T)
-mousex <- unique(genesV2[, 2])
+mousex = getLDS(attributes = c("hgnc_symbol"), filters = "hgnc_symbol", values = x , mart = human, attributesL = c("mgi_symbol"), martL = mouse, uniqueRows=T)[,2]
 return(mousex)
 }
  
