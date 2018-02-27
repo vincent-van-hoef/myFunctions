e5e920c0edccb8579dee5c5b72126c091231882e
u0049379 <u0049379@gbw-s-seq08.luna.kuleuven.be>
2018-02-27 16:14:53 +0100

    Add the plotSignature function

--- R/plotSignature.R
+++ R/plotSignature.R
@@ -1,3 +1,11 @@
+#' A function to plot the ecdf of a list of signature genesets in a dataset as well as the complementary background ecdf.
+#' @description Provide a dataframe x containing the statistic to be plotted (e.g. logFC) and a list of signatures in the same format as the rownames of the data frame. The ecdf of the rownames of the df x belonging to the signature will be plotted as well as the complementary background.
+#' @param x Dataframe of matrix containing the value to be used for the ecdf calculation
+#' @param effCol colname of number of column of the value to be used for the ecdf calculation
+#' @param signatureList Named list of signatures, vector in same format as rownames of x
+#' @param xlab Label of the ecdf plot x-axis
+#' @param xlim Limits of the x-axis of the plot
+#' @export
 function(x, effCol, signatureList, xlab, xlim){
     all <- rownames(x)
     for(signature in 1:length(signatureList)){
