a57e6ba7967079b312b5c570aa51a9258cb2efc2
u0049379 <vvanhoef@hotmail.com>
2018-02-28 09:05:41 +0100

    uniqueRows FALSE to TRUE

--- R/humanMouseConversion.R
+++ R/humanMouseConversion.R
@@ -1,9 +1,10 @@
 #' Basic function to convert human to mouse gene names
 #' @export
 convertHumanGeneList <- function(x){
-human = useMart("ensembl", dataset = "hsapiens_gene_ensembl")
-mouse = useMart("ensembl", dataset = "mmusculus_gene_ensembl")
-mousex = getLDS(attributes = c("hgnc_symbol"), filters = "hgnc_symbol", values = x , mart = human, attributesL = c("mgi_symbol"), martL = mouse, uniqueRows=FALSE)[,2]
+human 	<- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+mouse 	<- useMart("ensembl", dataset = "mmusculus_gene_ensembl")
+tmp 	<- getLDS(attributes = c("hgnc_symbol"), filters = "hgnc_symbol", values = x , mart = human, attributesL = c("mgi_symbol"), martL = mouse, uniqueRows=TRUE)
+mousex 	<- unique(tmp[,2])
 return(mousex)
 }
  
