7e7833154b16dd6a2ff8db06e90600ad857cd561
u0049379 <u0049379@gbw-s-seq08.luna.kuleuven.be>
2018-02-28 09:00:12 +0100

    uniqueRows FALSE to TRUE

--- R/mouseHumanConversion.R
+++ R/mouseHumanConversion.R
@@ -3,8 +3,9 @@
 #' @param x A vector of mouse genes
 #' @export 
 convertMouseGeneList <- function(x){
-human = useMart("ensembl", dataset = "hsapiens_gene_ensembl")
-mouse = useMart("ensembl", dataset = "mmusculus_gene_ensembl")
-humanx = getLDS(attributes = c("mgi_symbol"), filters = "mgi_symbol", values = x , mart = mouse, attributesL = c("hgnc_symbol"), martL = human, uniqueRows=FALSE)[,2]
+human 	<- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+mouse 	<- useMart("ensembl", dataset = "mmusculus_gene_ensembl")
+tmp 	<- getLDS(attributes = c("mgi_symbol"), filters = "mgi_symbol", values = x , mart = mouse, attributesL = c("hgnc_symbol"), martL = human, uniqueRows=TRUE)
+humanx 	<- unique(tmp[,2])
 return(humanx)
 }
